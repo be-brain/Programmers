44b8e9fed03a1db17e12bc9df950f76f5f53b0e5
Chaeha Kim <113012150+be-brain@users.noreply.github.com>
2023-04-24 05:15:00 +0900

    [unrated] Title: 덧칠하기, Time: 7215.44 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,11 +1,17 @@
 function solution(n, m, section) {
-  let answer = 0;
-  let count = 0;
-  section.forEach((item) => {
-    if (item > count) {
-      answer++;
-      count = item + m - 1;
+    // n의 length만큼의 벽이 있다(1~8)
+    // section의 value가 필요 = 칠해야할 벽면번호
+    // 벽면갯수와 m(롤러길이)를 비교
+    const arr = Array.from({ length: n }, (_, i) => i + 1);
+    // arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (section.includes(arr[i]) && !result.includes(arr[i])) {
+            const a = arr.slice(i, i + m);
+            result.push(...a);
+        }
     }
-  });
-  return answer;
+    const answer = Math.ceil(result.length / m);
+    return answer;
 }
