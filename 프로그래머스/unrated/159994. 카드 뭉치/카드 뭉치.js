6d91c8aa6dabfc29b8b78c80b022d21a29bd551b
Chaeha Kim <113012150+be-brain@users.noreply.github.com>
2023-04-27 00:32:43 +0900

    [unrated] Title: 카드 뭉치, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,8 +1,18 @@
 function solution(cards1, cards2, goal) {
-    var answer = 'Yes';
-    const c1 = goal.filter(c => cards1.includes(c))
-    const c2 = goal.filter(c => cards2.includes(c))
-    c1.forEach((c, i) => { if (c != cards1[i]) { answer = "No"; } })
-    c2.forEach((c, i) => { if (c != cards2[i]) { answer = "No"; } })
-    return answer;
+    const a = [];
+    const b = [];
+    // goal에서 요소가 cards1,2에 있는지 확인
+    goal.map((item) => {
+        if (cards1.includes(item)) {
+            a.push(item);
+        } else if (cards2.includes(item)) {
+            b.push(item);
+        } else {
+            return "No";
+        }
+    });
+    return cards1.join("").includes(a.join("")) &&
+        cards2.join("").includes(b.join(""))
+        ? "Yes"
+        : "No";
 }
